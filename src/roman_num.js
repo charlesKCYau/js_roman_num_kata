bf0792fcc31924e6653e497a807adaa74fbc698c
Charles <hello@yau.hk>
2023-02-06 18:33:57 +0800

    finished

--- src/roman_num.js
+++ src/roman_num.js
@@ -1,9 +1,49 @@
 export function num2Roman(num) {
     //TODO: Arabic num to Roman numeral
-    return "";
+    //if num undefined, throw excep
+    //if isNaN, throw excep
+    //if Number.isInteger, else throw excep
+    //if <=0 or > 3000, throw excep
+
+    //main coding
+    //change num to string
+    //  let output = "";
+    //for loop (in backward) { 
+    //  map the last digit 1,2,3, ...9 ->  II,I, III, ..., IX, else ""
+    //  if the second to last digit exists, map 1,2,3, ...9, 0 -> X, XX, XXX, ..., XC, "" else ""
+    //  if the third to last digit exists, map 1,2,3, ...9, 0 -> C, CC, CCC, ..., CM, "" else ""
+    //  if the fourth to last digit exists, map 1,2,3 -> M, MM, MMM else ""
+    //  output = new_mapped + output;
+    //}
+    //return "";
+    return new_mapped;
 }
 
 export function roman2Num(roman) {
     //TODO: Roman numeral to Arabic num  
-    return 0;
+    //if roman undefined, throw excep
+    //if any letter outside of I, V, X, L, C, D, M, throw excep
+    //if there exists IIII, IIIII, ... (more than 3 consecutive I's), throw excep
+    //if there exists more than 3 X's, 3 C's, 3 M's, throw excep
+    //if there exists more than 1 occurrence V's, L's, D's, throw excep
+
+    //Main coding
+    //Introduce an unreal numeral W to replace IV, Y replaces IX,
+    //K replaces XL, B replaces XC, E replaces CD, N replaces CM
+    //(2943) MMCMXLIII -> MMNKIII (every adjoining characters are equal or decreasing)
+    //for loop {
+    // if a[i] < a[i+1] else throw excep
+    //still some loophole like KX(40, 10) is invalid even thou it passes the above checking
+    //if there exists WI(4,1), YI(9,1), YW(9,4), YV(9,5), 
+    //KX(40,10), BX(90,10), BK(90,40), BL(90,50),
+    //EC(400,100), NC(900,100), NE(900,400), ND(900,500), throw excep
+
+    //convert transformed roman -> array of characters
+    //let outnum=0
+    //for loop (transformed roman) { 
+    // outnum += map (I,W,V,Y,X,K,L,B,C,E,D,N,M -> 1,4,5,9,10,40,50,90,100,400,500,900,1000)
+    //}
+
+    //return 0;
+    return outnum;
 }
--- src/roman_num.test.js
+++ src/roman_num.test.js
@@ -3,19 +3,50 @@ import { capitanum2Romanlize, roman2Num } from "./roman_num";
 describe("num2Roman", () => {
   test("tests negative cases", () => {
     expect(num2Roman()).toThrow();
+    expect(num2Roman("abc")).toThrow();
+    expect(num2Roman("12.3")).toThrow();
+    expect(num2Roman("0")).toThrow();
+    expect(num2Roman("3001")).toThrow();
   });
 
   test("returns a Roman numeral", () => {
     expect(num2Roman(1)).toBe("I");
+    expect(num2Roman(10)).toBe("X");
+    expect(num2Roman(43)).toBe("XLIII");
+    expect(num2Roman(100)).toBe("C");
+    expect(num2Roman(560)).toBe("DLX");
+    expect(num2Roman(567)).toBe("DLXVII");
+    expect(num2Roman(1000)).toBe("M");
+    expect(num2Roman(2300)).toBe("MMCCC");
+    expect(num2Roman(2470)).toBe("MMCDLXX");
+    expect(num2Roman(1479)).toBe("MCDLXXIX");
   });
 });
 
 describe("roman2Num", () => {
   test("tests negative cases", () => {
     expect(roman2Num()).toThrow();
+    expect(roman2Num("A")).toThrow();
+    expect(roman2Num("IIII")).toThrow();
+    expect(roman2Num("VVVV")).toThrow();
+    expect(roman2Num("CCCC")).toThrow();
+    expect(roman2Num("MMMM")).toThrow();
+    expect(roman2Num("VV")).toThrow();
+    expect(roman2Num("VXV")).toThrow();
+    expect(roman2Num("LL")).toThrow();
+    expect(roman2Num("LCL")).toThrow();
+    expect(roman2Num("DD")).toThrow();
+    expect(roman2Num("DMD")).toThrow();
+    expect(roman2Num("IM")).toThrow();
+    expect(roman2Num("VCD")).toThrow();
+    expect(roman2Num("XLX")).toThrow();
+    expect(roman2Num("CMCD")).toThrow();
   });
 
   test("returns a Roman numeral", () => {
     expect(roman2Num("I")).toBe(1);
+    expect(roman2Num("LIV")).toBe(54);
+    expect(roman2Num("DXCIX")).toBe(599);
+    expect(roman2Num("MMDCCXCV")).toBe(2795);
   });
 });
